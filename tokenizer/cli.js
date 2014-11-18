397ef04ca3999a4070fbad48c0a153696e31087d
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2014-11-18 03:39:21 -0800

    Detect globals.

--- tokenizer/cli.js
+++ tokenizer/cli.js
@@ -16,16 +16,37 @@ process.stdin.on('readable', function () {
 });
 
 process.stdin.on('end', function () {
-    var tokens;
+    var data, tokens;
 
     // Generate a syntax tree for the input.
     JSLINT(whole);
+    data = JSLINT.data();
 
     // Minimize an otherwise-circular structure.
-    tokens = JSLINT.data().tokens.map(function (token) {
+    tokens = data.tokens.map(function (token) {
+        var origin = token,
+            level;
+
+        // We always consider the function keyword to be "part" of the scope it
+        // creates, even if the name leaks in the case of function statements.
+        if (token.kind !== 'function') {
+            // Find a variable/parameter's origin.
+            while (origin.master) {
+                origin = origin.master;
+            }
+        }
+
+        // Globality is not indicated by origin function.
+        if (token.kind !== 'function' &&
+                data.global.indexOf(token.string) > -1) {
+            level = 0;
+        } else {
+            level = origin.function.level;
+        }
+
         return {
             from: token.from,
-            level: token.function.level,
+            level: level,
             line: token.line,
             thru: token.thru
         };
