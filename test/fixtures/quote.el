af4f773cabd9deccfabae2a4947178697f60defc
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2015-05-30 14:43:48 -0700

    Colorize comments and strings inside backquotes.

--- test/fixtures/quote.el
+++ test/fixtures/quote.el
@@ -1,7 +1,9 @@
 (defun a (a)
   (or (eq a 'b)
       (equal a '(a b))
-      (equal a `(,(append () `(a b ,(+ 1 free) ,free b) free) b ,free))))
+      (equal a `(,(append () `(a b ,(+ 1 free) ,free b) free) b ,free
+                 "s" ; c
+                 ))))
 
 (append '("a" ; b
           "b" ; a
