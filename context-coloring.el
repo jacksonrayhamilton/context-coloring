32b276e96118f9e34f4cf9a5a2ae6cae3e772144
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2015-06-21 14:11:07 -0700

    Version 7.0.0.

--- context-coloring.el
+++ context-coloring.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2014-2015  Free Software Foundation, Inc.
 
 ;; Author: Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
-;; Version: 6.5.0
+;; Version: 7.0.0
 ;; Keywords: convenience faces tools
 ;; Package-Requires: ((emacs "24.3") (js2-mode "20150126"))
 ;; URL: https://github.com/jacksonrayhamilton/context-coloring
