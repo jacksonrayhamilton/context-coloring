d9c171b6337f372e3c797a5b6e400fce6aa43675
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2016-04-30 19:04:15 -0700

    Handle multiple files in coverage reports.

--- test/context-coloring-coverage.el
+++ test/context-coloring-coverage.el
@@ -111,14 +111,14 @@
    (mapcar
     #'context-coloring-coverage-format-source-file
     (cdr (assq 'source_files coverage-data)))
-   "\n"))
+   "\n\n"))
 
 (defun context-coloring-coverage-local-init ()
   "Initialize test coverage for local viewing."
   (make-directory context-coloring-coverage-output-directory t)
   (setq undercover-force-coverage t)
   (setenv "COVERALLS_REPO_TOKEN" "noop")
-  (undercover "context-coloring.el"
+  (undercover "*.el"
               (:report-file context-coloring-coverage-output-file)
               (:send-report nil))
   (add-hook
@@ -147,7 +147,7 @@
 
 (defun context-coloring-coverage-ci-init ()
   "Initialize test coverage for continuous integration."
-  (undercover "context-coloring.el")
+  (undercover "*.el")
   (require 'context-coloring))
 
 (provide 'context-coloring-coverage)
