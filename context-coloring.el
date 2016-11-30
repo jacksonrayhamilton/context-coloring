5fe3695baeeae04761828ff36b391a77edefdfe9
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2016-11-30 07:02:28 -0800

    Revert "Clean up local variable."
    This reverts commit 6a2c6fee3eb0bdc896e5fdf7e673192b4ebe778a.
    
    This broke Emacs Lisp docstring fontification.

--- context-coloring.el
+++ context-coloring.el
@@ -442,7 +442,6 @@ Feature inspired by Douglas Crockford."
         (let ((teardown (plist-get dispatch :teardown)))
           (when teardown
             (funcall teardown)))))
-    (kill-local-variable 'font-lock-syntactic-face-function)
     (turn-on-font-lock-if-desired))))
 
 (provide 'context-coloring)
